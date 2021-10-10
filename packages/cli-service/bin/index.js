01e0a552c512847748ca329b3bd8f7fd6f538da9
姜通 <jiangtong@heimaqf.com>
2021-10-10 20:24:05 +0800

    lerna

---
+++ packages/cli-service/bin/index.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const path = require('path');
+const devConfig = path.join(process.cwd(), 'build/webpack.config.dev.js');
+const prodConfig = path.join(process.cwd(), 'build/webpack.config.prod.js');
+
+const args = process.argv.slice(2);
+if (args[0] === 'start') {
+    const compiler = webpack(devConfig);
+    const server = new WebpackDevServer(compiler);
+
+    server.listen(3000, '0.0.0.0', (err) => {
+        console.log(err);
+    });
+} else if (args[0] === 'build') {
+    webpack(prodConfig, (err, stats) => {
+        if (err) console.log(err);
+        if (stats.hasErrors()) {
+            console.log(new Error('Build failed with errors.'));
+        }
+    });
+} else {
+    console.log('error command');
+}
